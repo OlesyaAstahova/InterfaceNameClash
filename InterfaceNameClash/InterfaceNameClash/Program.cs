31934b781ff121b1ac5b2bdc746267d0ac066623
Олесуа и Юля <astahova.98@gmail.com>
2018-03-06 09:32:18 +0700

    Неявная реализация интерфейсов

--- InterfaceNameClash/InterfaceNameClash/Program.cs
+++ InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -27,17 +27,17 @@ namespace InterfaceNameClash
             Octagon oct = new Octagon();
             IDrawToForm itfForm = (IDrawToForm)oct;
             itfForm.Draw();
-
+            /*
             ((IDrawToPrinter)oct).Draw();
 
             if (oct is IDrawToMemory)
                 ((IDrawToMemory)oct).Draw();
-
-            /*IDrawToPrinter itfPriner = (IDrawToPrinter)oct;
+                */
+            IDrawToPrinter itfPriner = (IDrawToPrinter)oct;
             itfPriner.Draw();
 
             IDrawToMemory itfMemory = (IDrawToMemory)oct;
-            itfMemory.Draw();*/
+            itfMemory.Draw();
 
             Console.ReadLine();
         }
