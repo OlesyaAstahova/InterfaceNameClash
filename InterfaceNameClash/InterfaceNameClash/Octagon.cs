31537d749404dfd1f91a084197fbd3e57d985e2c
Олесуа и Юля <astahova.98@gmail.com>
2018-03-06 09:29:38 +0700

    Явная реализация интерфейсов

---
+++ InterfaceNameClash/InterfaceNameClash/Octagon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing the Octagon...");
+        }
+    void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Drawing to form...");
+        }
+    void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to memory...");
+        }
+    void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to a printer...");
+        }
+    }
+}
---
+++ InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    public interface IDrawToForm
+    {
+        void Draw();
+    }
+    public interface IDrawToMemory
+    {
+        void Draw();
+    }
+    public interface IDrawToPrinter
+    {
+        void Draw();
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Interface Name Clashes *****\n");
+            Octagon oct = new Octagon();
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+
+            ((IDrawToPrinter)oct).Draw();
+
+            if (oct is IDrawToMemory)
+                ((IDrawToMemory)oct).Draw();
+
+            /*IDrawToPrinter itfPriner = (IDrawToPrinter)oct;
+            itfPriner.Draw();
+
+            IDrawToMemory itfMemory = (IDrawToMemory)oct;
+            itfMemory.Draw();*/
+
+            Console.ReadLine();
+        }
+      
+
+   }
+}
